f6b060c006ce49b0d65d746d9b3e32db28d70c5f
Nobody <mishasikolenko@mail.ru>
2024-01-23 12:30:42 +0300

    subplots@

--- Overfitting/L2-regularizer.py
+++ Overfitting/L2-regularizer.py
@@ -1,14 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def __main__():
-    x = np.arange(0, 10.1, 0.1)
-    y = np.array([a ** 3 - 10 * a ** 2 + 3 * a + 500 for a in x])  #x^3 - 10x^2 + 3x + 500
-    x_train, y_train = x[::2], y[::2]
-    N = 13  
-    L = 20
+def L2(L: int, x_values):
+    y = np.array([a ** 3 - 10 * a ** 2 + 3 * a + 500 for a in x_values])  # x^3 - 10x^2 + 3x + 500
+    x_train, y_train = x_values[::2], y[::2]
+    N = 13
 
-    X = np.array([[a ** n for n in range(N)] for a in x])
+    X = np.array([[a ** n for n in range(N)] for a in x_values])
     IL = np.array([[L if i == j else 0 for j in range(N)] for i in range(N)])
     IL[0][0] = 0
     X_train = X[::2]
@@ -20,8 +18,20 @@ def __main__():
     print(w)
 
     yy = [np.dot(w, x) for x in X]
-    plt.plot(x, yy)
-    plt.plot(x, y)
+
+    plt.plot(x_values, yy, label = f'L = {L}') 
+
+
+def __main__():
+    x_values = np.arange(0, 10.1, 0.1)
+    L_values = [5, 10, 20, 25]
+
+    for L in L_values:
+        L2(L, x_values)
+
+    y_original = [a ** 3 - 10 * a ** 2 + 3 * a + 500 for a in x_values]
+    plt.plot(x_values, y_original, label = 'Original Function')
+    plt.legend()
     plt.grid(True)
     plt.show()
 
